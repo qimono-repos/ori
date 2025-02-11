8a06f8c36aed0b43ec9f506fc6ba453651a82830
sat-lino-au <alberto.gruning.zen@gmai.com>
2025-02-11 03:39:20 +0000

    feat: add stiles for dark theme

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Say Hello to QiMono</Text>
+      <Text style={styles.text}>🙈 Say Hello to QiMono !!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#253030',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color:'white',
+    fontSize:20,
+  }
 });
